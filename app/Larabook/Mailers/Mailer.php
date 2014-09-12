42ff13981e4ad4ec8e950d012b85040a5770edaf
Elvin <elvis.mahmudov@gmail.com>
2014-09-12 10:55:04 +0000

    27 lesson Larabook

---
+++ app/Larabook/Mailers/Mailer.php
@@ -0,0 +1,27 @@
+<?php  namespace Larabook\Mailers; 
+
+use Illuminate\Mail\Mailer as Mail;
+
+abstract class Mailer {
+
+    public $mail;
+
+    public function __construct(Mail $mail)
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * @param $user
+     * @param $subject
+     * @param $view
+     * @param $data
+     */
+    public function sendTo($user, $subject, $view, $data = [])
+    {
+        return $this->mail->queue($view, $data, function($message) use($user, $subject)
+        {
+            $message->to($user->email)->subject($subject);
+        });
+    }
+} 
--- app/Larabook/Users/User.php
+++ app/Larabook/Users/User.php
@@ -5,7 +5,7 @@ use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
 use Eloquent, Hash;
-use Larabook\Registration\Events\UserRegistered;
+use Larabook\Registration\Events\UserHasRegistered;
 use Laracasts\Commander\Events\EventGenerator;
 use Laracasts\Presenter\PresentableTrait;
 
@@ -66,7 +66,7 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
     {
         $user = new static(compact('username', 'email', 'password'));
 
-        $user->raise(new UserRegistered($user));
+        $user->raise(new UserHasRegistered($user));
 
         return $user;
     }
