df6d687f2f28901f330773249468620abe50cf2b
Elvin <elvis.mahmudov@gmail.com>
2014-09-18 09:35:47 +0000

    add SendMessageCept

---
+++ tests/functional/SendMessageCept.php
@@ -0,0 +1,22 @@
+<?php 
+$I = new FunctionalTester($scenario);
+
+$I->am('a Larabook user');
+$I->wantTo('send a message to another Larabook user');
+
+//setup
+$I->haveAnAccount(['username' => 'OtherUser']);
+
+//actions
+$I->signIn();
+$I->click('Messages');
+$I->seeCurrentUrlEquals('/inbox');
+$I->fillField('Send to:', 'OtherUser');
+$I->fillField('Message:','some random message');
+$I->click('Send');
+
+//expectations
+$I->seeCurrentUrlEquals('/inbox');
+$I->see('OtherUser');
+$I->click('OtherUser');
+$I->see('some random message');
